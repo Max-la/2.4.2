681a92ca203d9c2fce8f1c465ad2bb19ea47ad8d
Maksim Gudkov <85955257+Max-la@users.noreply.github.com>
2021-11-08 15:32:51 +0300

    Delete user.sql

--- src/main/resources/user.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO mydb.roles (id, name) VALUES (1,'USER');
-INSERT INTO mydb.roles (id, name) VALUES (2,'ADMIN');
-insert into mydb.users (lastName, name, password) VALUES ('admin','admin','admin');
-INSERT INTO mydb.users_roles (users_id, roles_id) VALUES ( 1, 2 );
